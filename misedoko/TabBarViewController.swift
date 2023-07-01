11d29d249a8d0f8678d961a6eb1cab6d81d2f484
sakihosonuma <chipdale411@gmail.com>
2023-07-01 20:58:04 +0900

    tabbarcontrollerを書いたよ

---
+++ misedoko/TabBarViewController.swift
@@ -0,0 +1,29 @@
+//
+//  TabBarViewController.swift
+//  misedoko
+//
+//  Created by saki on 2023/07/01.
+//
+
+import UIKit
+
+class TabBarViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        selectedIndex = 1
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
